c31b5fdde9706945d8c641c69a437b30cfbfdaeb
Edd Lonzanida <eddie.lonzanida@incenter.tech>
2019-12-09 07:49:43 +0800

    Updated EmailTemplateDetailsCreateResponse

--- Business/EcomEngine.Business.Common/Dto/EcomEngineDb/EmailTemplateDetailsCreateResponse.cs
+++ Business/EcomEngine.Business.Common/Dto/EcomEngineDb/EmailTemplateDetailsCreateResponse.cs
@@ -1,8 +1,36 @@
-﻿using EcomEngine.Business.Common.Entities.EcomEngineDb;
+﻿using EcomEngine.Business.Common.BaseClasses;
+using Eml.Contracts.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcomEngine.Business.Common.Dto.EcomEngineDb
 {
-    public class EmailTemplateDetailsCreateResponse : EmailTemplate
+    public class EmailTemplateDetailsCreateResponse : EntitySoftDeletableGuidBase, IEntityWithParentBase<Guid>
     {
+        public Guid ParentId { get; set; }
+
+        public string EmailLabel { get; set; }
+
+        [EmailAddress]
+        public string FromAddress { get; set; }
+
+        public string Subject { get; set; }
+
+        public string TemplateText { get; set; }
+
+        public string EmailType { get; set; }
+
+        public bool Active { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateUpdated { get; set; }
+
+        public bool LoadDrafts { get; set; }
+        public bool IsDefault { get; set; }
+
+        [EmailAddress]
+        public string BccAddress { get; set; }
+
+        public int VersionCount { get; set; }
     }
 }
